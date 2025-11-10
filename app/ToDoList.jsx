4ee3027a8609705e1e66322ad28ce3d5669af47a
Yusif Najafli <ynajafli@outlook.com>
2025-11-10 11:22:26 -0700

    removed styling

--- app/ToDoList.jsx
+++ app/ToDoList.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function ToDoList({ tasks }) {
   return (
-    <View style={{ padding: 20 }}>
+    <View>
       {tasks.map((task, index) => (
         <Text key={index}>
           {task}
